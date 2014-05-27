f48fb2ec4cc5e7086a96977de35741ebe5a326bf
Nguyễn Văn Được <nguyenvanduocit@gmail.com>
2014-05-27 11:11:00 +0700

    Update SingleTypeAdapter.java
    Add some usefull method like ArrayList : add, remove, addall, removeall

--- lib/src/main/java/com/github/kevinsawicki/wishlist/SingleTypeAdapter.java
+++ lib/src/main/java/com/github/kevinsawicki/wishlist/SingleTypeAdapter.java
@@ -32,152 +32,183 @@ import java.util.List;
  */
 public abstract class SingleTypeAdapter<V> extends TypeAdapter {
 
-  private static final Object[] EMPTY = new Object[0];
+    private final LayoutInflater inflater;
 
-  private final LayoutInflater inflater;
+    private final int layout;
 
-  private final int layout;
+    private final int[] children;
 
-  private final int[] children;
+    private final List<V> items = new ArrayList<V>();
 
-  private Object[] items;
+    /**
+     * Create adapter
+     *
+     * @param activity
+     * @param layoutResourceId
+     */
+    public SingleTypeAdapter(final Activity activity, final int layoutResourceId) {
+        this(activity.getLayoutInflater(), layoutResourceId);
+    }
 
-  /**
-   * Create adapter
-   *
-   * @param activity
-   * @param layoutResourceId
-   */
-  public SingleTypeAdapter(final Activity activity, final int layoutResourceId) {
-    this(activity.getLayoutInflater(), layoutResourceId);
-  }
+    /**
+     * Create adapter
+     *
+     * @param context
+     * @param layoutResourceId
+     */
+    public SingleTypeAdapter(final Context context, final int layoutResourceId) {
+        this(LayoutInflater.from(context), layoutResourceId);
+    }
 
-  /**
-   * Create adapter
-   *
-   * @param context
-   * @param layoutResourceId
-   */
-  public SingleTypeAdapter(final Context context, final int layoutResourceId) {
-    this(LayoutInflater.from(context), layoutResourceId);
-  }
+    /**
+     * Create adapter
+     *
+     * @param inflater
+     * @param layoutResourceId
+     */
+    public SingleTypeAdapter(final LayoutInflater inflater,
+                             final int layoutResourceId) {
+        this.inflater = inflater;
+        this.layout = layoutResourceId;
 
-  /**
-   * Create adapter
-   *
-   * @param inflater
-   * @param layoutResourceId
-   */
-  public SingleTypeAdapter(final LayoutInflater inflater,
-      final int layoutResourceId) {
-    this.inflater = inflater;
-    this.layout = layoutResourceId;
+        int[] childIds = getChildViewIds();
+        if (childIds == null)
+            childIds = new int[0];
+        children = childIds;
+    }
 
-    items = EMPTY;
+    /**
+     * Clear all items
+     *
+     * @return this adapter
+     */
+    public SingleTypeAdapter clear() {
+        items.clear();
 
-    int[] childIds = getChildViewIds();
-    if (childIds == null)
-      childIds = new int[0];
-    children = childIds;
-  }
+        notifyDataSetChanged();
+        return this;
+    }
 
-  /**
-   * Get a list of all items
-   *
-   * @return list of all items
-   */
-  @SuppressWarnings("unchecked")
-  protected List<V> getItems() {
-    List<? extends Object> objList = Arrays.asList(items);
-    return (List<V>) objList;
-  }
+    /**
+     * Add item to adapter registered as the given type
+     *
+     * @param item
+     * @return this adapter
+     */
+    public SingleTypeAdapter addItem(final V item) {
+        items.add(item);
 
-  /**
-   * Set items to display
-   *
-   * @param items
-   */
-  public void setItems(final Collection<?> items) {
-    if (items != null && !items.isEmpty())
-      setItems(items.toArray());
-    else
-      setItems(EMPTY);
-  }
+        notifyDataSetChanged();
+        return this;
+    }
 
-  /**
-   * Set items to display
-   *
-   * @param items
-   */
-  public void setItems(final Object[] items) {
-    if (items != null)
-      this.items = items;
-    else
-      this.items = EMPTY;
-    notifyDataSetChanged();
-  }
+    /**
+     * Add items to adapter registered as the given type
+     *
+     * @param items
+     * @return this adapter
+     */
+    public SingleTypeAdapter addItems(final Collection<V> items) {
+        if (items == null || items.size() == 0)
+            return this;
 
-  @Override
-  public int getCount() {
-    return items.length;
-  }
+        for (V item : items)
+            this.items.add(item);
 
-  @SuppressWarnings("unchecked")
-  public V getItem(final int position) {
-    return (V) items[position];
-  }
+        notifyDataSetChanged();
+        return this;
+    }
 
-  @Override
-  public long getItemId(final int position) {
-    return items[position].hashCode();
-  }
+    /**
+     * Add items to adapter registered as the given type
+     *
+     * @param items
+     * @return this adapter
+     */
+    public SingleTypeAdapter setItems(final Collection<V> items) {
+        if (items == null || items.size() == 0)
+            return this;
 
-  /**
-   * Get child view ids to store
-   * <p>
-   * The index of each id in the returned array should be used when using the
-   * helpers to update a specific child view
-   *
-   * @return ids
-   */
-  protected abstract int[] getChildViewIds();
+        this.items.clear();
+        addItems(items);
 
-  /**
-   * Initialize view
-   *
-   * @param view
-   * @return view
-   */
-  protected View initialize(final View view) {
-    return super.initialize(view, children);
-  }
+        notifyDataSetChanged();
+        return this;
+    }
+    /**
+     * Remove item at position
+     *
+     * @param position
+     * @return this adapter
+     */
+    public SingleTypeAdapter removeItem(final int position) {
+        if (position > 0 && position < items.size()
+                && items.remove(position) != null)
+            notifyDataSetChanged();
+        return this;
+    }
 
-  /**
-   * Update view for item
-   *
-   * @param position
-   * @param view
-   * @param item
-   */
-  protected void update(int position, View view, V item) {
-    setCurrentView(view);
-    update(position, item);
-  }
+    @Override
+    public int getCount() {
+        return items.size();
+    }
 
-  /**
-   * Update item
-   *
-   * @param position
-   * @param item
-   */
-  protected abstract void update(int position, V item);
+    @Override
+    public V getItem(final int position) {
+        return items.get(position);
+    }
 
-  @Override
-  public View getView(final int position, View convertView,
-      final ViewGroup parent) {
-    if (convertView == null)
-      convertView = initialize(inflater.inflate(layout, null));
-    update(position, convertView, getItem(position));
-    return convertView;
-  }
+    @Override
+    public long getItemId(final int position) {
+        return items.get(position).hashCode();
+    }
+
+    /**
+     * Get child view ids to store
+     * <p/>
+     * The index of each id in the returned array should be used when using the
+     * helpers to update a specific child view
+     *
+     * @return ids
+     */
+    protected abstract int[] getChildViewIds();
+
+    /**
+     * Initialize view
+     *
+     * @param view
+     * @return view
+     */
+    protected View initialize(final View view) {
+        return super.initialize(view, children);
+    }
+
+    /**
+     * Update view for item
+     *
+     * @param position
+     * @param view
+     * @param item
+     */
+    protected void update(int position, View view, V item) {
+        setCurrentView(view);
+        update(position, item);
+    }
+
+    /**
+     * Update item
+     *
+     * @param position
+     * @param item
+     */
+    protected abstract void update(int position, V item);
+
+    @Override
+    public View getView(final int position, View convertView,
+                        final ViewGroup parent) {
+        if (convertView == null)
+            convertView = initialize(inflater.inflate(layout, null));
+        update(position, convertView, getItem(position));
+        return convertView;
+    }
 }
